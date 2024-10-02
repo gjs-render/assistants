fa8b4842a94cc99330185a7493d6f3ccf6518d7e
Gary James Stilwell <161110593+gjs-render@users.noreply.github.com>
2024-10-02 17:57:08 -0600

    Update render.py

--- render.py
+++ render.py
@@ -16,111 +16,74 @@ api_key = os.getenv('OPENAI_API_KEY')
 # Initialize the OpenAI client
 client = OpenAI(api_key=api_key)
 
+app = Flask(__name__)
+
 # Configure logging
-#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
+  
+##############################
+@app.route("/")
+def index():
+    return render_template("render.html")
 
-
-
-# Step 1: Create a new Assistant with File Search Enabled
+# STEP 1 - CREATE AND ASSISTANT
 assistant = client.beta.assistants.create(
-  name="Financial Analyst Assistant",
-  instructions="You are an expert financial analyst. Use you knowledge base to answer questions about audited financial statements.",
+  name="Math Tutor",
+  instructions="You are a personal math tutor. Write and run code to answer math questions.",
+  tools=[{"type": "code_interpreter"}],
   model="gpt-4o",
-  tools=[{"type": "file_search"}],
 )
 
 
-'''
-"name": "Web Development Assistant",
-"instructions": "You are an expert web developer. You can search for files, read and analyze the contents of HTML and CSS files, and propose revisions.",
-"model": "gpt-4o",
-"tools": [
-        {"type": "file_search"},  # Enables file search capabilities
-        {"type": "file_analysis", "languages": ["HTML", "CSS"]},  # Enables reading and analyzing HTML and CSS files
-        {"type": "code_suggestion", "languages": ["HTML", "CSS"]}  # Enables coding suggestions for HTML and CSS
-        }
-]
-'''
+# STEP 2 - CREATE A THREAD
+thread = client.beta.threads.create()
 
 
-# Step 2: Upload files and add them to a Vector Store
-# Create a vector store caled "Financial Statements"
-vector_store = client.beta.vector_stores.create(name="Financial Statements")
- 
-# Ready the files for upload to OpenAI
-file_paths = ["filePath/aapl-10K.pdf", "filePath/brka-10k.pdf"]
-file_streams = [open(path, "rb") for path in file_paths]
- 
-# Use the upload and poll SDK helper to upload the files, add them to the vector store,
-# and poll the status of the file batch for completion.
-file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-  vector_store_id=vector_store.id, files=file_streams
-)
- 
-# You can print the status and the file counts of the batch to see the result of this operation.
-# print(file_batch.status)
-# print(file_batch.file_counts)
-
-
-
-# Step 3: Update the assistant to use the new Vector Store
-assistant = client.beta.assistants.update(
-  assistant_id=assistant.id,
-  tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+# STEP 3 - ADD A MESSAGE TO A THREAD
+message = client.beta.threads.messages.create(
+  thread_id=thread.id,
+  role="user",
+  content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
 )
 
 
-
-# 04 Step 4: Create a thread
-# Upload the user provided file to OpenAI
-message_file = client.files.create(
-  file=open("filePath/aapl-10k.pdf", "rb"), purpose="assistants"
-)
- 
-# Create a thread and attach the file to the message
-thread = client.beta.threads.create(
-  messages=[
-    {
-      "role": "user",
-      "content": "How many shares of AAPL were outstanding at the end of of October 2023?",
-      # Attach the new file to the message.
-      "attachments": [
-        { "file_id": message_file.id, "tools": [{"type": "file_search"}] }
-      ],
-    }
-  ]
-)
- 
-# The thread now has a vector store with that file in its tool resources.
-#print(thread.tool_resources.file_search)
-
-
-
-# Step 5: Create a run and check the output
+# STEP 4 - CREATE A RUN
 from typing_extensions import override
-from openai import AssistantEventHandler, OpenAI
+from openai import AssistantEventHandler
  
-
-############################################
-
-class EventHandler(AssistantEventHandler):
-    @override
-    def on_text_created(self, text) → None:
-        print(f"\nassistant > “, end=”", flush=True)
-
-# Then, we use the stream SDK helper
-# with the EventHandler class to create the Run
-# and stream the response.run = None
-
-with self.client.beta.threads.runs.stream(
-        thread_id=thread_id,
-        assistant_id=assistant_id,
-        event_handler=EventHandler(),
-    ) as stream:
-        stream.until_done()
-        run = stream.get_final_run()
+# First, we create a EventHandler class to define
+# how we want to handle the events in the response stream.
  
-
-
-
-
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+  
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
+ 
+# Then, we use the `stream` SDK helper 
+# with the `EventHandler` class to create the Run 
+# and stream the response.
+ 
+with client.beta.threads.runs.stream(
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+  instructions="Please address the user as Jane Doe. The user has a premium account.",
+  event_handler=EventHandler(),
+) as stream:
+  stream.until_done()
